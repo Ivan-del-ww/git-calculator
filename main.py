445b495751173b2cf65df9d811f3921afa4200ab
BAHR <oskolkovivan10@gmail.com>
2025-05-31 02:20:39 +0700

    Update add function comment in feature/add

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 # Simple calculator\n
+
+def add(a, b):
+     # Функция сложения двух чисел
+    return a + b
+
+def subtract(a, b):
+     
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Error: Division by zero"
+    return a / b
